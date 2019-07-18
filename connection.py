2ae8beba3d7a64131f57633014df100b29cca5b1
MartinHarvey <gtb17178@uni.strath.ac.uk>
2019-07-18 11:11:35 +0100

    GUI works without terminal for potentially non-password protected connections. I need to generate a non protected file when I get my ovpn server back online

--- connection.py
+++ connection.py
@@ -1,26 +1,20 @@
+#!/usr/bin/env python3
 import subprocess
 from tkinter import filedialog
 
 class connection():
     def __init__(self):
         self.filePath = filedialog.askopenfilename(
-            initialdir="/home/", 
+            initialdir="/home/",
             title = "Open .ovpn file",
-            filetypes = (("ovpn files","*.ovpn"),
+            filetypes = (("ovpn files", "*.ovpn"),
                          ("all files", ".*"))
         )
 
-        proc = subprocess.Popen(['/usr/local/sbin/openvpn', 
-                                "--config", 
+        proc = subprocess.Popen(['/usr/sbin/openvpn',
+                                "--config",
                                 self.filePath])
-        proc.communicate()[0]
-        
-    
+        out = proc.communicate()[0]
+
     def disconnect(self):
         subprocess.run(["sudo", "killall", "openvpn"])
-
-
-    
-    
-
-        
--- window.py
+++ window.py
@@ -1,21 +1,23 @@
+#!/usr/bin/env python3
 from tkinter import Tk, Label, Button, Text
 from connection import *
 
+
 class Window:
     def __init__(self, master):
-        #Set a instance of command class to null for now
+        # Set a instance of command class to null for now
         self.con = None
-        #set the window title
+        # set the window title
         master.title("OpenVPN-GUI")
 
         self.openButton = Button(
-            master, 
+            master,
             text="Open File",
             command = lambda: Window.Open(self))
         self.openButton.pack()
 
         self.disconnectButton = Button(
-            master, 
+            master,
             text = "Disconnect from Server",
             command = lambda: Window.disconnect(self),
             state = 'disabled')
@@ -24,23 +26,24 @@ class Window:
         self.statusText = Label(
             master,
             height = 4,
-            width = 25           
+            width = 25
         )
         self.statusText.pack()
-    
+        '''
+        self.protocol(
+            master,
+            "WM_DELETE_WINDOW",
+            self.onExit()
+        )
+        '''
 
     def Open(self):
         self.con = connection()
         self.disconnectButton['state'] = 'normal'
-    
+
     def disconnect(self):
         self.con.disconnect()
-
     
-
-
-
-
-
-
-
+    def onExit(self):
+        self.disconnect()
+        self.destroy()
